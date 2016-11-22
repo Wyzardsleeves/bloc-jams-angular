a85b67586e202054419cdf0f173cdd42ae2fccc3
Justin L. Lattimore <jlattimore3d@gmail.com>
2016-11-22 02:22:53 -0500

    completed checkpoint-5-controllers

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+        { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+        { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+        { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+        { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+    ]
+};
+
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumNotorious = {
+    title: 'Born Again',
+    artist: 'Notorious B.I.G.',
+    label: 'EM',
+    year: '1980',
+    albumArtUrl: 'https://pbs.twimg.com/profile_images/1968025269/NotoriousBIG.jpg',
+    songs: [
+        { title: 'Big Pappa', duration: '1:01' },
+        { title: 'Hypnotize', duration: '5:01' },
+        { title: 'Juicy', duration: '3:21'},
+        { title: 'Old thing back', duration: '3:14' },
+        { title: 'Mo money mo problems', duration: '2:15'}
+    ]
+};
