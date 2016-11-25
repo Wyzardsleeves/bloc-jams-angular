0e351845255cf3cde6346b10c07f9a4d8091a782
Justin L. Lattimore <jlattimore3d@gmail.com>
2016-11-25 03:18:55 -0500

    completed checkpoint-6-servicesPart1

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,4 +1,19 @@
 (function(){
+    function CollectionCtrl(Fixtures){
+        this.albums = [];
+        for(var i = 0; i < 12; i++){
+            this.albums.push(Fixtures.getAlbum());
+        }
+    }
+
+    angular
+        .module('blocJams')
+        .controller('Fixtures', CollectionCtrl);
+})();
+
+/* Old code
+
+(function(){
     function CollectionCtrl(){
         this.albums = [];
         for(var i = 0; i < 12; i++){
@@ -11,3 +26,4 @@
         .controller('CollectionCtrl', CollectionCtrl);
 })();
 
+*/
