85c75cf1e0e1de8b8e0e17c8b0d4baf145d9c08d
Justin L. Lattimore <jlattimore3d@gmail.com>
2016-11-30 05:30:45 -0500

    completed assignment-7-servicesPart2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -12,13 +12,8 @@
         /**
         *@function setSong
         *@desc Stops currently playing song and Loads new audio file as currentBuzzObject
-        *@param {Object} song
+        *@param {Object} data
         */
-        var playSong = function(){
-            currentBuzzObject.play();
-            
-        };
-        
         var setSong = function(data){
             if(currentBuzzObject){
                 currentBuzzObject.stop();
@@ -33,93 +28,39 @@
             currentSong = data;
         };
         
-        SongPlayer.play = function(data) {
-            if(currentSong !== data){
-                
-                setSong(data);    
-                currentBuzzObject.play();
-                data.playing = true;
-            
-            }else if(currentSong === data){
-                if(currentBuzzObject.isPaused()){
-                    currentBuzzObject.play();
-                }
-            }
-        };
-        
-        SongPlayer.pause = function(data){
-            currentBuzzObject.pause();
-            data.playing = false;
-        };
-        
-        return SongPlayer;
-    }
-    
-    angular
-        .module('blocJams')
-        .factory('SongPlayer', ['Fixtures', SongPlayer]);
-})();
-
-/* Buttons functioning but not re-usable
-(function(){
-    function SongPlayer(Fixtures){
-        var SongPlayer = {};
-        
-        var currentSong = null;
-        var currentBuzzObject = null;
-        
-        SongPlayer.play = function(data) {
-            if(currentSong !== data){
-                if(currentBuzzObject){
-                    currentBuzzObject.stop();
-                    currentSong.playing = null;
-                }
-                
-                currentBuzzObject = new buzz.sound(data.audioUrl, {
-                    formats: ['mp3'],
-                    preload: true
-                });
-                
-                currentSong = data;
-    
-                currentBuzzObject.play();
-                data.playing = true;
-            
-            }else if(currentSong === data){
-                if(currentBuzzObject.isPaused()){
-                    currentBuzzObject.play();
-                }
-            }
-        };
-        
-        SongPlayer.pause = function(data){
-            currentBuzzObject.pause();
-            data.playing = false;
-        };
-        
-        return SongPlayer;
-    }
-    
-    angular
-        .module('blocJams')
-        .factory('SongPlayer', ['Fixtures', SongPlayer]);
-})();
-
-*/
-
-/*
-
-(function(){
-    function SongPlayer(Fixtures){
-        var SongPlayer = {};
-        
-        SongPlayer.play = function(data) {
-            var currentBuzzObject = new buzz.sound(data.audioUrl, {
-                formats: ['mp3'],
-                preload: true
-            });
-            
+        //assignment
+        var playSong = function(data){
             currentBuzzObject.play();
+            data.playing = true;
+        };
+        
+        /**
+        *@SongPlayer method
+        *@desc Pulls a song to play from albums.html
+        *@param {Object} data
+        */        
+        SongPlayer.play = function(data) {
+            if(currentSong !== data){
+                
+                setSong(data);
+                //assignment
+                playSong(data);
+            
+            }else if(currentSong === data){
+                if(currentBuzzObject.isPaused()){
+                    currentBuzzObject.play();
+                }
+            }
+        };
+        
+        /**
+        *@function setSong
+        *@desc Stops currently playing song and Loads new audio file as currentBuzzObject
+        *@param {Object} data
+        */
+        SongPlayer.pause = function(data){
+            currentBuzzObject.pause();
+            data.playing = false;
         };
         
         return SongPlayer;
@@ -130,4 +71,3 @@
         .factory('SongPlayer', ['Fixtures', SongPlayer]);
 })();
 
-*/
